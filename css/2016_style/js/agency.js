1e5b3be0779eb7c7386acb0c368d768bbdd7300b
gruvi-repo <32784097+gruvi-repo@users.noreply.github.com>
2018-09-06 15:41:03 -0700

    Update agency.js

--- css/2016_style/js/agency.js
+++ css/2016_style/js/agency.js
@@ -8,6 +8,7 @@
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
+	console.log(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
